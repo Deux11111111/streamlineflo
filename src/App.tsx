9e36f9cbc6f958e703a581b2cd0f8ac65e088334
Duex12 <deuxtraep@gmail.com>
2025-10-03 23:48:56 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import { Helmet } from "react-helmet";
+import { HelmetProvider } from "react-helmet-async";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
